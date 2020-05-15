9173aca562d9a18162432bfa8d80d000aa46f1d5
leaf-junkie <ragsdale.jar@gmail.com>
2020-05-15 14:08:00 -0400

    update style - remove advocates and testimonials sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,9 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    console.log( "ready!" );
 
-    $("#loginBtn").on("click", function() {
+    $("#loginBtn").on("click", function(event) {
+        event.preventDefault();
         $("#loginModal").show();
     });
 
-//     setTimeout(function() {
-//         // code to be executed after 5 seconds
-//         $("#testimonial1").fadeOut();
-//         $("#testimonial2").fadeIn();
-//     }, 5000);
-            
-//     setTimeout(function() {
-//         // code to be executed after 10 seconds
-//         $("#testimonial2").fadeOut();
-//         $("#testimonial3").fadeIn();
-//     }, 10000);
-
-//     setTimeout(function() {
-//         // code to be executed after 15 seconds
-//         $("#testimonial3").fadeOut();
-//         $("#testimonial1").fadeIn();
-//     }, 15000);
 });
