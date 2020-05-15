d3079a7d4a7d5d7798ddc96aa7d9ae484436bafd
leaf-junkie <ragsdale.jar@gmail.com>
2020-05-15 12:25:15 -0400

    update assets and layout

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    $("#loginBtn").on("click", function() {
+        $("#loginModal").show();
+    });
+
+//     setTimeout(function() {
+//         // code to be executed after 5 seconds
+//         $("#testimonial1").fadeOut();
+//         $("#testimonial2").fadeIn();
+//     }, 5000);
+            
+//     setTimeout(function() {
+//         // code to be executed after 10 seconds
+//         $("#testimonial2").fadeOut();
+//         $("#testimonial3").fadeIn();
+//     }, 10000);
+
+//     setTimeout(function() {
+//         // code to be executed after 15 seconds
+//         $("#testimonial3").fadeOut();
+//         $("#testimonial1").fadeIn();
+//     }, 15000);
+});
